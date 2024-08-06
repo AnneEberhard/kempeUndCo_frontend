afe0018a8d966b2cfd68c4d4a93c915120b1ac54
AnneEberhard <anne.eberhard@gmx.net>
2024-08-06 16:27:28 +0200

    first commit

---
+++ src/app/ancestors/ancestors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ancestors',
+  standalone: true,
+  imports: [],
+  templateUrl: './ancestors.component.html',
+  styleUrl: './ancestors.component.scss'
+})
+export class AncestorsComponent {
+
+}
