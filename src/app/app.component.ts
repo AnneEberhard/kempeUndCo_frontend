2672e0ec1ebe0eb4703b172942fbe342ff71df3c
AnneEberhard <anne.eberhard@gmx.net>
2024-08-06 17:22:44 +0200

    registered modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DiscussionsComponent } from './discussions/discussions.component';
+import { InfosComponent } from './infos/infos.component';
+import { AncestorsComponent } from './ancestors/ancestors.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { LegalComponent } from './legal/legal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    RegistrationComponent,
+    LoginComponent,
+    WelcomeComponent,
+    DiscussionsComponent,
+    InfosComponent,
+    AncestorsComponent,
+    RecipesComponent,
+    LegalComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
