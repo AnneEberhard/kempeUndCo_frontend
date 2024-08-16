1431ced7f1a4d57cb42a758a9feec9b0284c54e1
AnneEberhard <anne.eberhard@gmx.net>
2024-08-16 10:46:42 +0200

    basic html layout for ancestors

--- src/app/ancestors/ancestors.component.ts
+++ src/app/ancestors/ancestors.component.ts
@@ -14,17 +14,72 @@ export class AncestorsComponent {
   family = {
     grandparents: [
       { name: 'Großvater 1' },
-      { name: 'Großmutter 1' }
+      { name: 'Großmutter 1' },
+      { name: 'Großvater 2' },
+      { name: 'Großmutter 2' }
     ],
     parents: [
       { name: 'Vater' },
       { name: 'Mutter' }
     ],
     person: { name: 'Person X' },
-    spouse: { name: 'Ehepartner' },
-    children: [
-      { name: 'Kind 1' },
-      { name: 'Kind 2' }
+    marriages: [
+      {
+        spouse: { name: 'Drost von Hülshoff dolude whatever' },
+        children: [
+          { name: 'Kind 1' },
+          { name: 'Kind 2' }
+        ]
+      },
+      {
+        spouse: { name: 'Ehepartner' },
+        children: [
+          { name: 'Kind 1' },
+          { name: 'Kind 2' }
+        ]
+      },
+      {
+        spouse: { name: 'Ehepartner' },
+        children: [
+        ]
+      }
     ]
   };
+
+
+
+    /**
+  * shows overlay with hints
+  */
+    showHint() {
+      const div = document.getElementById('popUpHintContainer');
+      if (div) {
+        div.classList.remove('dNone');
+      }
+      }
+    
+      /**
+      * hides overlay with hints
+      */
+      hideHint() {
+        const div = document.getElementById('popUpHintContainer');
+        if (div) {
+          div.classList.add('dNone');
+        }
+      }
+
+      showInfos() {
+        console.log('infos');
+        const div = document.getElementById('popUpInfoContainer');
+        if (div) {
+          div.classList.remove('dNone');
+        }
+      }
+
+      hideInfos() {
+        const div = document.getElementById('popUpInfoContainer');
+        if (div) {
+          div.classList.add('dNone');
+        }
+      }
 }
