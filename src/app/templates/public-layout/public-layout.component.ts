30cfc72be055eceb5eb14b82b2f9ef6f2b7af7db
AnneEberhard <anne.eberhard@gmx.net>
2026-07-29 11:39:59 +0200

    Angular 19 läuft

--- src/app/templates/public-layout/public-layout.component.ts
+++ src/app/templates/public-layout/public-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
-import { ScrollToTopButtonComponent } from '../scroll-to-top-button/scroll-to-top-button.component';
 import { PublicHeaderComponent } from "../public-header/public-header.component";
 
 @Component({
     selector: 'app-public-layout',
-    imports: [RouterOutlet, RouterLink, PublicHeaderComponent, FooterComponent, ScrollToTopButtonComponent, PublicHeaderComponent],
+    standalone: true,
+    imports: [RouterOutlet, PublicHeaderComponent, FooterComponent, PublicHeaderComponent],
     templateUrl: './public-layout.component.html',
     styleUrl: './public-layout.component.scss'
 })
