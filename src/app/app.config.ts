e5e316081cbbd5767f68cc8f62f7b94a4d77f647
AnneEberhard <anne.eberhard@gmx.net>
2026-08-07 15:47:35 +0200

    update fast-uri

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
-import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Router } from '@angular/router';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { LOCALE_ID } from '@angular/core';
 import { authInterceptor } from './services/auth.interceptor';
 import { QuillModule } from 'ngx-quill';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import * as Sentry from "@sentry/angular";
-import { headerInterceptor } from './services/header.interceptor';
 import { inject, provideAppInitializer } from '@angular/core';
 // added here: provideHttpClient(withInterceptors([authInterceptor, headerInterceptor])), causes CORS Error
 
